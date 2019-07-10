6a45e8dc7aa9ad7223189550315d3d716bf7e138
Somfic <somfic@gmail.com>
2019-07-10 14:36:59 +0200

    Init

---
+++ CoinflipTactics/CoinFlipper.cs
@@ -0,0 +1,15 @@
+﻿using CoinflipTactics.Tactics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinflipTactics
+{
+    public static class CoinFlipper
+    {
+        public static void Bet(ITactic tactic, bool heads, ulong amount)
+        {
+
+        }
+    }
+}
---
+++ CoinflipTactics/Tactics/ITactic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinflipTactics.Tactics
+{
+    public interface ITactic
+    {
+        void Flip();
+
+        ulong Balance { get; set; }
+    }
+}
